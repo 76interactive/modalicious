29b4218d09e1b6cf0f092880fae2cc68ee061d38
Sjors Snoeren <hello@sjors.me>
2016-02-22 09:51:25 +0100

    Adds native back button functionality and adds afterPresentModal callback

--- src/jquery.modalicious.js
+++ src/jquery.modalicious.js
@@ -2,7 +2,7 @@
  * modalicious
  *
  * @author Sjors Snoeren
- * @version 0.1.1
+ * @version 0.1.2
  */
 ;(function($) {
 
@@ -10,10 +10,12 @@
 
     this.settings = {};
 
+    var _this = this;
+
     var defaults = {
 
       /**
-       * Body backgroundColor isn't used while showing a modal you 
+       * Body backgroundColor isn't used while showing a modal you
        * can define a custom backgroundColor to show
        */
       backgroundColor: '#000000',
@@ -27,20 +29,28 @@
        * When dismissing, a new state is pushed using the baseURL
        * defaults to root
        */
-      historyBaseURL: '/'
+      historyBaseURL: '/',
 
-    };  
-    
+      /**
+       * Enable override on the built-in back and forward functionality
+       * of a browser
+       */
+      usesNativeBackAndForward: true
+
+      /**
+       * Callback after opening
+       */
+      afterPresentModal: null
+    };
+
     this.init = function() {
-      var _this = this;
-
       this.settings = $.extend({}, defaults, options);
 
       if (!$('.modalicious--wrapper').length) {
         this.insertWrapperPlaceholder();
       }
 
-      $(element).on('click', function(e) {
+      $(element).on('click.modalicous', function(e) {
         e.preventDefault();
 
         var href = $(this).attr('href');
@@ -48,9 +58,11 @@
       });
     };
 
-    this.bindModalEvents = function() {
-      var _this = this;
+    this.didPopState = function(e) {
+      _this.dismissModal();
+    };
 
+    this.bindModalEvents = function() {
       $('.modalicious--close').on('click', function(e) {
         e.preventDefault();
 
@@ -59,7 +71,6 @@
     };
 
     this.presentModal = function(href) {
-      var _this = this;
       var $wrapper = $('.modalicious--wrapper');
 
       $wrapper.load(href, function(response) {
@@ -70,6 +81,14 @@
 
         if (_this.settings.shouldPushHistory) {
           history.pushState({}, '', href);
+        }        
+
+        if (_this.settings.usesNativeBackAndForward) {
+          $(window).bind('popstate', _this.didPopState);
+        }
+
+        if (_this.settings.afterPresentModal) {
+          _this.settings.afterPresentModal(href, $wrapper);
         }
 
         $(document).trigger('modalicious_load');
@@ -84,12 +103,16 @@
         history.pushState({}, '', this.settings.historyBaseURL);
       }
 
+      if (_this.settings.usesNativeBackAndForward) {
+        $(window).unbind('popstate', this.didPopState);
+      }
+      
       $(document).trigger('modalicious_unload');
     };
 
     this.insertWrapperPlaceholder = function() {
       $('body').append('<div class="modalicious--wrapper" style="width: 100%; position: fixed; overflow: auto; top: 0; left: 0; bottom: 0; z-index: 999;"></div>');
-      
+
       var $wrapper = $('.modalicious--wrapper');
       $wrapper.css('background-color', this.settings.backgroundColor);
       $wrapper.hide();
@@ -102,14 +125,13 @@
     this.setScrollEnabled = function(enabled) {
       $('body').css('overflow', enabled ? 'auto' : 'hidden');
     };
-
-    this.init();
   }
 
   $.fn.modalicious = function(options) {
     return this.each(function() {
       if (undefined == $(this).data('modalicious')) {
         var plugin = new $.modalicious(this, options);
+        plugin.init();
         $(this).data('modalicious', plugin);
       }
     });
